5692dad465c99e8e64fc7cf6e4be09054a23700f
md-rehman <rehman.2468@gmail.com>
2024-11-15 06:44:10 +0530

    vercel.json update and mf update

--- apps/p5-playground/src/pages/index.tsx
+++ apps/p5-playground/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import dynamic from "next/dynamic";
 import { exampleSketch } from "../components";
 
@@ -11,15 +10,9 @@ const P5Wrapper = dynamic(
 export default function Home() {
   return (
     <main className="flex flex-1 h-screen w-screen flex-col justify-center items-center">
+      <p>Below you should see a simple p5 example.</p>
       <P5Wrapper sketch={exampleSketch} />
-      <Image
-        className="dark:invert"
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        priority
-      />
+      <p>Above you should see a simple p5 example.</p>
     </main>
   );
 }
--- apps/tv-set/next.config.mjs
+++ apps/tv-set/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     APP_P5_PLAYGROUND: process.env.APP_P5_PLAYGROUND,
   },
   remotes: {
-    "p5-playground": `p5-playground@${process.env.APP_P5_PLAYGROUND}_next/static/chunks/remoteEntry.js`,
+    "p5-playground": `p5-playground@https://www.md-rehman.dev/p5-playground/_next/static/chunks/remoteEntry.js`,
   },
 };
 
