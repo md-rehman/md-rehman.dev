d9643c7cdbc686981d15142b9dbdc2e9827bc51f
md-rehman <rehman.2468@gmail.com>
2024-11-18 15:33:43 +0530

    remove custom vercel config

--- apps/p5-playground/src/components/p5-sketches/example.tsx
+++ apps/p5-playground/src/components/p5-sketches/example.tsx
@@ -5,7 +5,7 @@ export const exampleSketch: Sketch = (p5) => {
   p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
 
   p5.draw = () => {
-    p5.background(0, 0, 0, 0);
+    p5.background(0, 0, 0, 100);
     p5.normalMaterial();
     p5.push();
     p5.rotateZ(p5.frameCount * 0.01);
