5fe4002ba3695560ff88a6cc2805be4a96608681
md-rehman <rehman.2468@gmail.com>
2026-08-21 19:00:12 +0530

    feat(home): set shades-of-grey as default theme

--- apps/home/app/components/ThemeSwitcher.tsx
+++ apps/home/app/components/ThemeSwitcher.tsx
@@ -1,1 +1,5 @@
-export { ThemeSwitcher } from "@repo/atomic-ui/molecules";
+import { ThemeSwitcher as AtomicThemeSwitcher } from "@repo/atomic-ui/molecules";
+
+export function ThemeSwitcher() {
+  return <AtomicThemeSwitcher defaultTheme="shades-of-grey" />;
+}
